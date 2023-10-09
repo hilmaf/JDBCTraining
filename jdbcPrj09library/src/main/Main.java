ca43716d1c5ebc6b4023fa7dcc84b317a392b429
ORLANDO\shico <glee1470@naver.com>
2023-10-09 22:16:44 +0900

    도서 관리 프로그램
    세팅

---
+++ jdbcPrj09library/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("<< 도서 대출 및 반납 프로그램 >>");
+		// - 도서 검색
+		// - 도서 목록 조회
+		// - 도서 정보 수정
+		// - 신규도서 등록
+		// - 도서 대출 및 반납
+		
+	}
+
+}
